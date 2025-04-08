22393dea8c4fa130bf6bd8a3b131eed83a2419fa
shovo404 <shovo15-5456@diu.edu.bd>
2025-04-09 01:02:34 +0600

    13

--- lib/main.dart
+++ lib/main.dart
@@ -9,43 +9,64 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ContainerExample(),
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      title: 'Custom AppBar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CustomAppBarScreen(),
     );
   }
 }
 
-class ContainerExample extends StatelessWidget {
-  const ContainerExample({super.key});
+class CustomAppBarScreen extends StatelessWidget {
+  const CustomAppBarScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(40),
-          margin: const EdgeInsets.all(30),
-          decoration: BoxDecoration(
-            color: Colors.blue[300], 
-            borderRadius: BorderRadius.circular(16), 
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26, 
-                blurRadius: 10,
-                offset: const Offset(4, 4),
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue,
+        centerTitle: true,
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.network(
+                'https://media.istockphoto.com/id/1620926852/photo/white-sneaker-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=CcYj5SYufcuFxUy-l9u1pgDbz62Ty3aD_FaCpqBCaM8=',
+                height: 35,
               ),
-            ],
-          ),
-          child: const Text(
-            "Container!",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
             ),
+            const SizedBox(width: 10),
+            const Text(
+              'Sneaker\'s Heaven',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              debugPrint('Search pressed');
+            },
           ),
+          IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              debugPrint('Menu pressed');
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'This is a custom AppBar!',
+          style: TextStyle(fontSize: 20),
         ),
       ),
     );
