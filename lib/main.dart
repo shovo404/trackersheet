c8afbe4da0724bd29cab70d0409d56b115ff9f0b
shovo404 <shovo15-5456@diu.edu.bd>
2025-04-09 01:00:20 +0600

    12

--- lib/main.dart
+++ lib/main.dart
@@ -9,63 +9,43 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const ImageFromAssetsScreen(),
+    return const MaterialApp(
+      home: ContainerExample(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class ImageFromAssetsScreen extends StatelessWidget {
-  const ImageFromAssetsScreen({super.key});
+class ContainerExample extends StatelessWidget {
+  const ContainerExample({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Forest Image Example',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 30,
+      backgroundColor: Colors.grey[200],
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(40),
+          margin: const EdgeInsets.all(30),
+          decoration: BoxDecoration(
+            color: Colors.blue[300], 
+            borderRadius: BorderRadius.circular(16), 
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26, 
+                blurRadius: 10,
+                offset: const Offset(4, 4),
+              ),
+            ],
           ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              'Forest View',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.green,
-              ),
+          child: const Text(
+            "Container!",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
             ),
-            const SizedBox(height: 10),
-            const Text(
-              'A beautiful and serene view of a lush green forest.',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20),
-            Image.asset(
-              'images/forest.png',  // Correct asset path
-              width: 350,
-              height: 250,
-              fit: BoxFit.cover,
-            ),
-          ],
+          ),
         ),
       ),
     );
