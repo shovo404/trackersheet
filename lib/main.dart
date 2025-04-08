38205aeff79e3b61daa90fdea6a0664005c2f21a
shovo404 <shovo15-5456@diu.edu.bd>
2025-04-08 23:20:32 +0600

    04

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,74 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const StyledTextApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class StyledTextApp extends StatelessWidget {
+  const StyledTextApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: ListView.builder(
-          itemCount: 50,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: Center(
-                child: Text("Item $index"),
+      debugShowCheckedModeBanner: false,
+      home: const TextStyleScreen(),
+    );
+  }
+}
+
+class TextStyleScreen extends StatelessWidget {
+  const TextStyleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Shahriar Ahmed Shovo',
+              style: TextStyle(
+                fontSize: 38,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 12, 5, 110),
               ),
-            );
-          },
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Shahriar Ahmed Shovo',
+              style: TextStyle(
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+                color: Colors.green,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Shahriar Ahmed Shovo',
+              style: TextStyle(
+                fontSize: 10,
+                decoration: TextDecoration.underline,
+                color: Color.fromARGB(255, 154, 55, 147),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  'Mobile Application',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.orange,
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
