89b0f9cbc887f9c357009140218126d83e289040
shovo404 <shovo15-5456@diu.edu.bd>
2025-04-08 23:17:34 +0600

    03

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,27 @@
 import 'package:flutter/material.dart';
- 
- void main() {
-   runApp(const TrackerSheet());
- }
- 
- class TrackerSheet extends StatelessWidget {
-   const TrackerSheet({super.key});
- 
-   @override
-   Widget build(BuildContext context) {
-     return MaterialApp(
-       debugShowCheckedModeBanner: false,
-       home: const HomeScreen(),
-     );
-   }
- }
- 
- class HomeScreen extends StatefulWidget {
-   const HomeScreen({super.key});
- 
-   @override
-   State<HomeScreen> createState() => _HomeScreenState();
- }
- 
- class _HomeScreenState extends State<HomeScreen> {
-   String displayText = 'Hello World!';
- 
-   void _changeText() {
-     setState(() {
-       displayText = 'Pressed';
-     });
-   }
- 
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
-       backgroundColor: const Color.fromARGB(255, 215, 227, 234),
-       body: Center(
-         child: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: [
-             Text(
-               displayText,
-               style: const TextStyle(
-                 fontSize: 32,
-                 fontWeight: FontWeight.bold,
-                 color: Color.fromARGB(255, 11, 11, 11),
-                 fontFamily: 'DMSans',
-               ),
-             ),
-             const SizedBox(height: 20),
-             ElevatedButton(
-               style: ElevatedButton.styleFrom(
-                 backgroundColor: const Color.fromARGB(255, 29, 147, 244),
-                 shape: RoundedRectangleBorder(
-                   borderRadius: BorderRadius.circular(60), 
-                 ),
-               ),
-               onPressed: _changeText,
-               child: const Text(
-                 'Press Here',
-                 style: TextStyle(
-                   fontSize: 32,
-                   color: Color.fromARGB(255, 240, 243, 240), 
-                   fontWeight: FontWeight.bold,
-                 ),
-               ),
-             ),
-           ],
-         ),
-       ),
-     );
-   }
- }
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView.builder(
+          itemCount: 50,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Center(
+                child: Text("Item $index"),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
